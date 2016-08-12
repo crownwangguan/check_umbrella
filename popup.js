16321d672c7a79f4737b533396f5cf18966f7170
crownwangguan <crown.wg@outlook.com>
2016-08-12 19:27:17 -0400

    working fine

--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+chrome.browserAction.onClicked.addListener(function(tab) {
     var currentLocaiton;
     var promise = $.getJSON("http://ipinfo.io");
     promise.done(function(data){
@@ -11,14 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
             var categories = ["scattered thunderstorms", "rain", "thunderstorms", "scattered showers", "scattered snow showers", "snow showers"];
             var todayWeather = response.query.results.channel.item.forecast[0].text.toLowerCase();
             if($.inArray(todayWeather, categories) > -1) {
-                window.alert('Remember to bring an umbrella!');
+                alert('Remember to bring an umbrella!')
             } else {
-                window.alert('It\'s fine, enjoy your day!');
+                alert('It\'s fine, enjoy your day!');
             }
         });
     });
     promise.fail(function(err){
         console.log(err);
     });
-  }, false);
-}, false);
+});
